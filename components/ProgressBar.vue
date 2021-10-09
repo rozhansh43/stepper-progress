8a80feecb15ea4d0a88031aab74762580b184b20
rozhan sheikholmoluki <rozhansheikholmoluki@gmail.com>
2021-10-09 13:08:01 +0330

    init files

---
+++ components/ProgressBar.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <div class="wrapper-mains">
+      <div class="left-align">
+        <h2 class="bold center" style="color:#3aac5d;">Make Commponent Step Progress in Vue.js</h2>
+        <StepProgress :length="2"  @gonext="gonext" />/>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+ methods: {
+    gonext () {
+      this.items.push({
+        
+      })
+    },
+  }
+}
+</script>
+(html comment removed:  Add "scoped" attribute to limit CSS to this component only )
+<style scoped>
+.wrapper-mains{
+  width: 561px;
+    height: auto;
+    background: white;
+    text-align: center;
+    margin: auto;
+    padding: 100px 47px 20px 61px;
+}
+.textarea{
+    display: block;
+    width: 100%;
+    height: 70px;
+    border-radius: 5px;
+}
+</style>
---
+++ components/StepProgress.vue
@@ -0,0 +1,143 @@
+<template>
+  <div class="root">
+     <div class="container">
+          <ul class="progressbar">
+            <li :class="[isActive ? activeClass : '', disableClass]" :key="i"> </li>
+          </ul>
+          <div class="page1" v-if="length==1">
+            PAGE 1
+            <button class="bg-light" @click="goback">
+              back
+            </button>
+            <button class="bg-light" @click="gonext">
+              next
+            </button>
+          </div>
+          <div class="page3" v-if="length==2">
+            PAGE 2
+             <button class="bg-light" @click="goback">
+              back
+            </button>
+            <button class="bg-light" @click="gonext">
+              next
+            </button>
+          </div>
+          <div class="page3" v-if="length==3">
+            PAGE 3
+             <button class="bg-light" @click="goback">
+              back
+            </button>
+            <button class="bg-light" @click="gonext">
+              next
+            </button>
+          </div>
+          <div class="page3" v-if="length==4">
+            PAGE 4
+             <button class="bg-light" @click="goback">
+              back
+            </button>
+            <button class="bg-light" @click="gonext">
+              next
+            </button>
+          </div>
+      </div>
+  </div>
+</template>
+<script>
+export default {
+
+  data () {
+    return {
+      length:4,
+      isActive: true ,
+      activeClass: 'active',
+      disableClass: 'disable'
+    }
+  },
+  methods: {
+    goback () {
+      if (this.length <= 4 ) {
+      this.length = this.length - 1
+      }
+    },
+    gonext () {
+      if (this.length <= 3 ) {
+        this.length = this.length + 1
+      }
+    }
+  }
+}
+</script>
+
+(html comment removed:  Add "scoped" attribute to limit CSS to this component only )
+<style scoped>
+.bg-light {
+  background: lightgray;
+}
+
+ul {
+  list-style-type: none;
+}
+.container{
+     width: 35%;
+    position: absolute;
+    z-index: 1;
+    margin-top: 20px;
+}
+.progressbar{
+  counter-reset: step;
+}
+.progressbar li{
+  float: left;
+  width: 14.28%;
+  position: relative;
+  text-align: center;
+}
+.progressbar li:before{
+  content:counter(step);
+  counter-increment: step;
+  width: 30px;
+  height: 30px;
+  border: 2px solid #bebebe;
+  display: block;
+  margin: 0 auto 10px auto;
+  border-radius: 50%;
+  line-height: 27px;
+  background: white;
+  color: #bebebe;
+  text-align: center;
+  font-weight: bold;
+}
+.progressbar li:after{
+  content: '';
+  position: absolute;
+  width:100%;
+  height: 3px;
+  background: #979797;
+  top: 15px;
+  left: -50%;
+  z-index: -1;
+}
+.progressbar li.active:before{
+ border-color: #3aac5d;
+ background: #3aac5d;
+ color: white
+}
+.progressbar li.active:after{
+ background: #3aac5d;
+}
+.progressbar li.active - li:after{
+ background: #3aac5d;
+}
+.progressbar li.active - li:before{
+border-color: #3aac5d;
+background: #3aac5d;
+color: white
+}
+.progressbar li:first-child:after{
+ content: none;
+}
+.disableClass {
+  background-color: blue;
+}
+</style>
